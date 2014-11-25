d9e05dc4003a75a06668aa76b9c909d364c82032
Jimmy F <james.ferrara@gmail.com>
2014-11-24 21:21:14 -0500

    More work: cv2hough

--- src/cv2hough.py
+++ src/cv2hough.py
@@ -1,12 +1,17 @@
+"""
+cv2 test code: pulls out circles in images (bottle caps) and saves them off
+into smaller image files for later analysis and rearrangement.
+
+NOTE:
+OpenCV and/or cv/cv2 is a real hassle to install.  This project is wonky
+because it doesn't use a virtualenv as my system refuses to accept OpenCV for
+a virtualenv.  Be careful!
+"""
+
 import cv2
 from cv2 import cv
-import sys
 from os.path import join, isdir
 from os import makedirs
-
-
-def err_msg(msg):
-    print str(msg)
 
 
 def cv2hough(sobel_image, color_image, minrad, maxrad):
@@ -19,7 +24,7 @@ def cv2hough(sobel_image, color_image, minrad, maxrad):
     """
 
     in_cv_image = cv2.imread(sobel_image, 0)
-    avg_rad = (minrad+maxrad)/2
+    avg_rad = (minrad + maxrad) / 2
     circles = cv2.HoughCircles(in_cv_image, cv.CV_HOUGH_GRADIENT, 1, 65,
                                param1=90, param2=15,
                                minRadius=minrad, maxRadius=maxrad)
@@ -40,8 +45,8 @@ def export_circles(circ_data, src_img, out_dir, avg_rad):
     if not isdir(join('.', out_dir)):
         try:
             makedirs(join('.', out_dir))
-        except:
-            err_msg("Couldn't make directory.")
+        except OSError:
+            print "Couldn't make directory"
             exit(-1)
     count = 0
     for k in circ_data[0]:
@@ -51,31 +56,7 @@ def export_circles(circ_data, src_img, out_dir, avg_rad):
         ## offset of y - for some reason the hough transform is offset by 20?
         xo = 0
         ## offset for x - in case we need it some day
-        cv2.imwrite(join('.', out_dir, "circle_"+str(count)+"_img.jpg"),
-                    src_img_ld[k[1]-(yo+rnd):k[1]+(rnd-yo),
-                    k[0]-(rnd+xo):k[0]+(rnd-xo)])
+        cv2.imwrite(join('.', out_dir, "circle_" + str(count) + "_img.jpg"),
+                    src_img_ld[k[1] - (yo + rnd):k[1] + (rnd - yo),
+                    k[0] - (rnd + xo):k[0] + (rnd - xo)])
         count += 1
-
-
-def helpMessage():
-    """Prints a help message for command line."""
-    print "[analyze] (filename)"
-
-
-def handleCommandLine():
-    """Handles command line arguments; redirected from __main__"""
-    if len(sys.argv) < 4:
-        helpMessage()
-        sys.exit()
-    valid_commands = set(['analyze'])
-    att_command = sys.argv[1].strip().lower()
-    if att_command == 'analyze':
-        if sys.argv[2].strip().lower()[-3:] == 'bmp':
-            cv2hough(sys.argv[2].strip(), sys.argv[3].strip(), 50, 60)
-    else:
-        helpMessage()
-        sys.exit()
-
-
-if __name__ == "__main__":
-    handleCommandLine()
