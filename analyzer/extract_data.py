d50093fee58ea736dc3a3d4498d719e6e4e37daa
jamesfe <james.ferrara@gmail.com>
2015-07-08 21:28:50 -0400

    Fixed the analyzer/extractor

--- analyzer/extract_data.py
+++ analyzer/extract_data.py
@@ -11,52 +11,89 @@ a virtualenv.  Be careful!
 import cv2
 from cv2 import cv
 from os.path import join, isdir
-from os import makedirs
+from os import makedirs, path
 
 
-def cv2hough(sobel_image, color_image, minrad, maxrad):
-    """
-    Identifies and cuts out to a directory potentially circular shapes
-    that match a known radius range.
-    :param sobel_image: sobel-filtered image
-    :param color_image: non-sobel filtered version of colored_image
-    :return:
-    """
+class CapImage:
 
-    in_cv_image = cv2.imread(sobel_image, 0)
-    avg_rad = (minrad + maxrad) / 2
-    circles = cv2.HoughCircles(in_cv_image, cv.CV_HOUGH_GRADIENT, 1, 65,
-                               param1=90, param2=15,
-                               minRadius=minrad, maxRadius=maxrad)
-    export_circles(circles, color_image, 'caps', avg_rad)
+    @staticmethod
+    def validate_paths(file_name, file_path):
+        if not path.isfile(path.join(file_path, file_name)):
+            raise ValueError
+        return True
 
+    def load_image(self):
+        self.loaded_image = cv2.imread(self.get_whole_target_path())
 
-def export_circles(circ_data, src_img, out_dir, avg_rad):
-    """
-    Export circles from an image (given hough xform) to a directory.
-    :param circ_data: output from cv2.HoughCircles
-    :param src_img: source, color image filename
-    :param out_dir: output directory
-    :param avg_rad: average radius of cap; user input
-    :return: <none>
-    """
+    def __init__(self, file_name=None, file_path="./", autoload=False, **kwargs):
+        if self.validate_paths(file_name, file_path):
+            self.file_name = file_name
+            self.file_path = file_path
 
-    src_img_ld = cv2.imread(src_img)
-    if not isdir(join('.', out_dir)):
-        try:
-            makedirs(join('.', out_dir))
-        except OSError:
-            print "Couldn't make directory"
-            exit(-1)
-    count = 0
-    for k in circ_data[0]:
-        rnd = avg_rad
-        ## average radius of circle (via user input)
-        yo = 20
-        ## offset of y - for some reason the hough transform is offset by 20?
-        xo = 0
-        ## offset for x - in case we need it some day
-        cv2.imwrite(join('.', out_dir, "circle_" + str(count) + "_img.jpg"),
-                    src_img_ld[k[1] - (yo + rnd):k[1] + (rnd - yo),
-                    k[0] - (rnd + xo):k[0] + (rnd - xo)])
-        count += 1
+        self.sobel_image = None
+        self.hough_circles = None
+        self.minrad = None
+        self.maxrad = None
+        self.average_rad = None
+
+        if autoload:
+            self.load_image()
+        else:
+            self.loaded_image = None
+
+        if 'param1' in kwargs:
+            self.param1 = kwargs['param1']
+        else:
+            self.param1 = 90
+
+        if 'param2' in kwargs:
+            self.param2 = kwargs['param2']
+        else:
+            self.param2 = 15
+
+    def get_whole_target_path(self):
+        return path.join(self.file_path, self.file_name)
+
+    def store_sobel_image(self):
+        sobel_x = cv2.Sobel(self.loaded_image, cv.CV_8U, 1, 0)
+        sobel_y = cv2.Sobel(self.loaded_image, cv.CV_8U, 0, 1)
+        self.sobel_image = cv2.bitwise_or(sobel_x, sobel_y)
+
+    def calculate_hough_circles(self, minrad, maxrad):
+        gray_image = cv2.cvtColor(self.sobel_image, cv2.COLOR_BGR2GRAY)
+        self.hough_circles = cv2.HoughCircles(gray_image, cv.CV_HOUGH_GRADIENT, 1, 65,
+                         param1=self.param1, param2=self.param2,
+                         minRadius=minrad, maxRadius=maxrad)
+        self.minrad = minrad
+        self.maxrad = maxrad
+        self.average_rad = sum([self.minrad, self.maxrad])/2
+
+    @staticmethod
+    def confirm_and_build_output_directory(output_directory):
+        if not isdir(join('.', output_directory)):
+            try:
+                makedirs(join('.', output_directory))
+                return True
+            except OSError:
+                return False
+        else:
+            return True
+
+    def export_circles(self, output_directory):
+        src_img_ld = self.loaded_image
+        if self.confirm_and_build_output_directory(output_directory):
+            for index, identified_circle in enumerate(self.hough_circles[0]):
+                rounding_val = self.average_rad
+                y_offset = 0
+                x_offset = 0
+                cv2.imwrite(join('.', output_directory, "circle_" + str(index) + "_img.jpg"),
+                            src_img_ld[identified_circle[1] - (y_offset + rounding_val):identified_circle[1] + (rounding_val - y_offset),
+                            identified_circle[0] - (rounding_val + x_offset):identified_circle[0] + (rounding_val - x_offset)])
+
+if __name__ == '__main__':
+    cap_filename = "P1000118.JPG"
+    cap_filepath = "../data/"
+    test_cap = CapImage(file_name=cap_filename, file_path=cap_filepath, autoload=True)
+    test_cap.store_sobel_image()
+    test_cap.calculate_hough_circles(40, 70)
+    test_cap.export_circles("output_dir")
